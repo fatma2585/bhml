a6740b421c03526bf77eee8a92105ea2c750b045
Fatma Shaban <67824021+fatma2585@users.noreply.github.com>
2024-07-12 15:17:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages, Extension
 
+# The text of the README file
+README = ("README.md").read_text()
 
 setup(
     name='bhml',
@@ -7,17 +9,20 @@ setup(
     packages=find_packages(),
     install_requires=["numpy>=1.24.4", "scipy>=1.11.1", "astropy>=5.3.3", "matplotlib>=3.6.1"]
 ,  # Add your dependencies here
-    description="constrain the evolution of a complete catalog based of redshift ranges and other paramters ranges (e.g. mass, luminosity .....)",
-    uathor='Fatma Shaban',
+    long_description=README,
+    description="This package is used to constrain huge Astronomical catalogs/surveys to subsets based on redshift ranges to study the evolution of celestial objects."
+    #Constrain the evolution of a complete catalog based on redshift ranges and other parameters ranges (e.g. mass, luminosity .....)",
+    author='Fatma Shaban',
     author_email='fatmashaban0@gmail.com',
     url='https://github.com/fatma2585/bhml',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Operating System:: OS Independent',
     ],
     python_requires='>=3.6',
     license="LICENSE",
     incluce_package_data=True,
+    
 
 )
